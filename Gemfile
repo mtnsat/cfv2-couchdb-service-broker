8e639f15f929f7fe761d2bc0469d40e715e57b98
Andres Rojas <andres.rojas@mtnsat.com>
2014-09-12 13:38:36 -0400

    Added basic settings and catalog support

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
+gem 'json', '~> 1.8.1'
 gem 'sinatra', '~> 1.4.5'
--- couchdb_broker.rb
+++ couchdb_broker.rb
@@ -1,4 +1,21 @@
 require 'sinatra'
+require 'json'
+require 'yaml'
+
+## Set if you want to switch the settings config
+# SETTINGS_FILENAME = 'config/foo.yml'
 
 class CouchDBBroker < Sinatra::Base
+  get '/catalog' do
+    content_type :json
+
+    self.class.app_settings.fetch('catalog').to_json
+  end
+
+  private
+
+  def self.app_settings
+    settings_filename = defined?(SETTINGS_FILENAME) ? SETTINGS_FILENAME : 'config/settings.yml'
+    @app_settings ||= YAML.load_file(settings_filename)
+  end
 end
