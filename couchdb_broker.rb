7556be1ab2640eb9031d1e597fb5f9b258e9f8f6
Andres Rojas <andres.rojas@mtnsat.com>
2014-09-14 16:41:18 -0400

    Shorter settings var

--- couchdb_broker.rb
+++ couchdb_broker.rb
@@ -9,13 +9,13 @@ class CouchDBBroker < Sinatra::Base
   get '/catalog' do
     content_type :json
 
-    self.class.app_settings.fetch('catalog').to_json
+    self.class.settings.fetch('catalog').to_json
   end
 
   private
 
-  def self.app_settings
-    settings_filename = defined?(SETTINGS_FILENAME) ? SETTINGS_FILENAME : 'config/settings.yml'
-    @app_settings ||= YAML.load_file(settings_filename)
+  def self.settings
+    file = defined?(SETTINGS_FILENAME) ? SETTINGS_FILENAME : 'config/settings.yml'
+    @settings ||= YAML.load_file(file)
   end
 end
